c11da6d23b7697072e3dbdccb14b6b2c28c18560
sarafrederixon <sarafrederixon@gmail.com>
2016-03-26 22:43:52 -0400

    so currently runs on dev but no routing set up yet, does not run for build yet

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Landing from "./components/Landing/Landing";
+
+ReactDOM.render(
+  <Landing/>,
+  document.body
+);
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  context:  __dirname + "/src",
+  entry: {
+    javascript: "./app.js",
+    html: "./index.html"
+  },
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist"
+  },
+  devtool: 'eval',
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'source-map'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.scss$/,
+        include: /src/,
+         loader: ExtractTextPlugin.extract(
+            "style",
+            "css!sass")
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'url?limit=8192',
+          'img'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: [
+          'react-hot',
+          'babel?presets[]=stage-0,presets[]=react,presets[]=es2015'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css")
+  ]
+}
