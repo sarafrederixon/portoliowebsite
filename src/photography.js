54b56f0664173cdbec4748a71c4766fa35463bef
Frederixon, Sara <s.frederixon@techsmith.com>
2016-04-17 14:04:52 -0400

    include router added a temp photography page, got build building out separate files for js and css based on name and a generated index file. Still need to get routes working and loading in scripts correctly

---
+++ src/photography.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Button from './components/Button';
+
+render(<Button value="BLAH"/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ var webpack = require('webpack');
 var path = require('path');
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const merge = require('webpack-merge');
 
@@ -11,32 +12,30 @@ const merge = require('webpack-merge');
 // Detect how npm is run and branch based on that
 const TARGET = process.env.npm_lifecycle_event;
 
-var BUILD_DIR = path.resolve(__dirname, 'build');
-var APP_DIR = path.resolve(__dirname, 'src');
+const PATHS = {
+  app: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build')
+};
 
 var config = {
    entry: {
-      javascript: APP_DIR + '/index.jsx',
-      html: "./src/index.html",
+     home: path.join(PATHS.app, 'home.js'),
+     photography: path.join(PATHS.app, 'photography.js')
    },
    output: {
-      path: BUILD_DIR,
-      filename: 'bundle.js'
+     path: PATHS.build,
+     filename: '[name].js'
    },
    module : {
       loaders : [
          {
             test : /\.jsx?/,
-            include : APP_DIR,
+            include : PATHS.app,
             loaders: ["react-hot", "babel"],
          },
          {
             test: /\.html$/,
             loader: "file?name=[name].[ext]",
-         },
-         {
-            test: /\.scss$/,
-            loaders: ["style", "css", "sass"]
          },
          {
             test: /\.(jpe?g|png|gif|svg)$/i,
@@ -46,7 +45,16 @@ var config = {
             ]
          },
       ]
-   }
+   },
+  plugins: [
+  new HtmlWebpackPlugin({
+    title: 'Sara Frederixon',
+    template: './src/my-index.ejs', // Load a custom template
+    inject: 'body', // Inject all scripts into the body
+    chunks: ['home']
+
+  })
+]
 };
 
 // Default configuration. We will return this if
@@ -78,6 +86,14 @@ if(TARGET === 'start' || !TARGET) {
       // If you want defaults, you can use a little trick like this
       // port: process.env.PORT || 3000
     },
+    module: {
+      loaders: [
+        {
+          test: /\.scss$/,
+          loaders: ["style", "css", "sass"]
+        }
+      ]
+    },
     plugins: [
       new webpack.HotModuleReplacementPlugin()
     ]
@@ -86,8 +102,17 @@ if(TARGET === 'start' || !TARGET) {
 
 if(TARGET === 'build') {
   module.exports = merge(config, {
+    module: {
+      loaders: [
+        {
+          test: /\.scss$/i,
+          loader: ExtractTextPlugin.extract(['css', 'sass'])
+        }
+      ]
+    },
     plugins: [
-      new CleanWebpackPlugin([BUILD_DIR]),
+      new ExtractTextPlugin("[name].css"),
+      new CleanWebpackPlugin([PATHS.build]),
 
       // Setting DefinePlugin affects React library size!
       // DefinePlugin replaces content "as is" so we need some
