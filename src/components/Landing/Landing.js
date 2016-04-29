c8fb92ff97ee01cfcf633cb340dd432b924ed62b
Frederixon, Sara <s.frederixon@techsmith.com>
2016-04-29 09:12:25 -0400

    slightly better javascript css

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -3,42 +3,53 @@ import Button from '../Button';
 require("./Landing.scss");
 
 var styles = {
-  body: {
+  html: {
     margin: 0,
     padding: 0,
 
-    height: 100,
+    height: '100%',
     overflow: 'hidden',
     WebkitTextSizeAdjust: 100,
     color: 'white',
  //   fontFamily: 'Poppins', 'Helvetica', sansSerif,
-
-    backgroundColor: 'linear-gradient(rgba(61, 66,90, 0.78),rgba(61, 66,90, 0.78))',
     backgroundImage: 'url('+'../../../photographs/FushimiInariLight.jpg'+')',
     backgroundRepeat: 'no-repeat',
-    backgroundPosition: "center center fixed",
+    backgroundPosition: "center center",
 
     WebkitBackgroundSize: 'cover',
     MozBackgroundSize: 'cover',
     OBackgroundSize: 'cover',
     backgroundSize: 'cover',
     position: 'fixed',
-    width: 100,
+    width: '100%',
 
     backgroundColor: "#454B66"
+  },
+  body: {
+    padding: 0,
+    margin: 0,
+    backgroundColor: 'rgba(61, 66, 90, 0.78)',
+    width: '100%',
+    height: '100%'
   }
 };
 
 var Landing = React.createClass({
     componentWillMount: function(){
+      for(var i in styles.html) {
+        document.documentElement.style[i] = styles.html[i];
+      };
       for(var i in styles.body){
         document.body.style[i] = styles.body[i];
-      }
+      };
     },
     componentWillUnmount: function(){
+      for(var i in styles.html) {
+        document.documentElement.style[i] = null;
+      };
       for(var i in styles.body){
         document.body.style[i] = null;
-      }
+      };
     },
   render: function() {
       return <div className="Landing-Content">
