a3e4e380d44e64f62b856466483b6b35645072fa
Frederixon, Sara <s.frederixon@techsmith.com>
2016-06-04 18:08:53 -0400

    trying to get mobile navigation working

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,18 +8,27 @@ var Navigation = React.createClass({
     return <div>
       <h1 className="Nav-Header">Sara Frederixon</h1>
       <div className="Link-Div">
-        <Link className="Link" to="/">
-          <span>Home</span>
-        </Link>
-        <Link className="Link" to="photography" activeClassName="active">
-          <span>Photography</span>
-        </Link>
-        <Link className="Link" to="projects" activeClassName="active">
-          <span>Projects</span>
-        </Link>
-        <Link className="Link" to="about" activeClassName="active">
-          <span>About</span>
-        </Link>
+        <label for="show-menu" className="show-menu">Show Menu</label>
+        <input type="checkbox" id="show-menu" role="button"/>
+          <ul id="menu">
+
+          <li> <Link className="Link" to="/">
+            <span>Home</span>
+          </Link> </li>
+
+          <li> <Link className="Link" to="photography" activeClassName="active">
+            <span>Photography</span>
+          </Link> </li>
+
+          <li> <Link className="Link" to="projects" activeClassName="active">
+            <span>Projects</span>
+          </Link> </li>
+
+          <li> <Link className="Link" to="about" activeClassName="active">
+            <span>About</span>
+          </Link> </li>
+
+        </ul>
       </div>
       <div className="hr-Div">
         <hr />
